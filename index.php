a57e172437e581809fe666a9b0e7ee43b4ff60d0
Douglas Barbosa <96439310+douttsdev@users.noreply.github.com>
2024-07-12 18:41:59 -0300

    Página de login

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="src/style.css"> 
+    
+</head>
+<body>
+ <div class="container"> 
+  <div class="card">
+  <h1>Tela de Login</h1>
+
+  <div class="label-float">
+    <input type="text" id="usuario" placeholder="">
+    <label for="usuario">Usuário</label>
+  </div>
+
+  <div class="label-float">
+    <input type="password" id="senha" placeholder="">
+    <label for="usuario">Senha</label>
+  </div>
+
+  <div class="justify-center">
+    <button>Entrar</button>
+  </div>
+
+  <div class="justify-center"> 
+    <hr>
+  </div>
+
+  <p> Não tem uma conta?
+  <a href=""> Cadastre-se </a>
+ </p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
+</div>
+                                                                                                                                                                                                                                                                                                                                                                                        
+</body>
+</html>
+
+
